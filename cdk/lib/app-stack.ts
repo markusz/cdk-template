9487d060f1f1ccf9df7ac955036dc974f170a395
Markus Ziller <mail.markus.ziller@gmail.com>
2022-05-13 10:27:30 +0200

    Adds Files

---
+++ cdk/lib/app-stack.ts
@@ -0,0 +1,12 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+interface AppStackProps extends StackProps {
+}
+
+export default class AppStack extends Stack {
+  constructor(scope: Construct, id: string, props?: AppStackProps) {
+    super(scope, id, props);
+
+  }
+}
